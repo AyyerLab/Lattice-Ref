9d79bbe013f15848019c6d55e11d218d614d994a
Abhishek Mall <mallabhi@ada01.bc.rzg.mpg.de>
2024-08-07 12:04:56 +0200

    print params

--- optimize_params.py
+++ optimize_params.py
@@ -1,6 +1,9 @@
 import numpy as np
-from scipy.optimize import minimize
 import h5py
+import sys
+import os
+
+from scipy.optimize import minimize
 import configparser
 
 class Optimizer:
@@ -17,8 +20,6 @@ class Optimizer:
             self.intens_vals = f['intens'][:]
             self.ftobj = f['ftobj'][:]
             self.funitc = f['funitc'][:]
-            self.shifts = f['shifts'][:]
-            self.fluence_vals = f['fluence'][:]
 
     def _getvals(self, array, h, k):
         qh = h + self.cen
@@ -26,61 +27,59 @@ class Optimizer:
         return array[qh, qk]
 
     def analyze_frame(self, intens, hk):
-        funitc_vals = [self._getvals(self.funitc, *val) for val in hk]
-        ftobj_vals = [self._getvals(self.ftobj, *val) for val in hk]
-        intens_vals = [self._getvals(intens, *val) for val in hk]
+        funitc_vals = np.array([self._getvals(self.funitc, *val) for val in hk])
+        ftobj_vals = np.array([self._getvals(self.ftobj, *val) for val in hk])
+        intens_vals = np.array([self._getvals(intens, *val) for val in hk])
 
         def objective(params):
             dx, dy, fluence = params
-            error = 0
-            for (h, k), funitc_val, ftobj_val, intens_val in zip(hk, funitc_vals, ftobj_vals, intens_vals):
-                qh = h
-                qk = k
-                phase = 2.0 * np.pi * (qh * dx + qk * dy)
-                pramp = np.exp(1j * phase)
-                model_int = fluence * np.abs(self.SCALE * funitc_val + ftobj_val * pramp) ** 2
-                error += (model_int - intens_val) ** 2
+            qh = np.array([h for h, k in hk])
+            qk = np.array([k for h, k in hk])
+            phase = 2.0 * np.pi * (qh * dx + qk * dy)
+            pramp = np.exp(1j * phase)
+            model_int = fluence * np.abs(self.SCALE * funitc_vals + ftobj_vals * pramp) ** 2
+            error = np.sum((model_int - intens_vals) ** 2)
             return error
 
-        ncoarse = 4
+        dx_range = np.linspace(0, 1, 4)
+        dy_range = np.linspace(0, 1, 4)
         fluence_range = np.arange(0, 10, 0.5)
-        dx_range = np.arange(0, 1, 1 / ncoarse)
-        dy_range = np.arange(0, 1, 1 / ncoarse)
+        dx_grid, dy_grid, fluence_grid = np.meshgrid(dx_range, dy_range, fluence_range)
 
+        params_grid = np.array([dx_grid.ravel(), dy_grid.ravel(), fluence_grid.ravel()]).T
         min_error = np.finfo('f8').max
         optimal_params = None
 
-        for dx in dx_range:
-            for dy in dy_range:
-                for fl in fluence_range:
-                    res = minimize(objective, (dx, dy, fl), method='Nelder-Mead')
-                    if res.fun < min_error:
-                        min_error = res.fun
-                        optimal_params = res.x
+        for params in params_grid:
+            res = minimize(objective, params, method='Nelder-Mead')
+            if res.fun < min_error:
+                min_error = res.fun
+                optimal_params = res.x
 
         return optimal_params, min_error
 
+    def _optimize_frame(self, frame_idx, hk):
+        intens = self.intens_vals[frame_idx]
+        optimal_params, min_error = self.analyze_frame(intens, hk)
+        result = {
+            'frame_idx': frame_idx,
+            'fitted_dx': optimal_params[0] % 1,
+            'fitted_dy': optimal_params[1] % 1,
+            'fitted_fluence': optimal_params[2] % 1,
+            'min_error': min_error
+        }
+        return result
+
     def optimize_params(self):
         hk = [(0, 1), (1, 1), (1, 0), (1, -1)]
+        frames = range(self.intens_vals.shape[0])
+
         results = []
-
-        for frame_idx in range(self.intens_vals.shape[0]):
-            intens = self.intens_vals[frame_idx]
-            optimal_params, min_error = self.analyze_frame(intens, hk)
-
-            result = {
-                'frame_idx': frame_idx,
-                'fitted_dx': optimal_params[0] % 1,
-                'fitted_dy': optimal_params[1] % 1,
-                'fitted_fluence': optimal_params[2] % 1,
-                'min_error': min_error
-            }
-
-            print(f"Frame {frame_idx}:")
-            print(f"Fitted: dx = {result['fitted_dx']}, dy = {result['fitted_dy']}, fl = {result['fitted_fluence']}")
-            print(f"Error: {min_error}\n")
-
+        for frame_idx in frames:
+            result = self._optimize_frame(frame_idx, hk)
             results.append(result)
+            print(f"Frame {frame_idx}: dx={result['fitted_dx']}, dy={result['fitted_dy']}, fluence={result['fitted_fluence']}, error={result['min_error']}", file=sys.stdout)
+            sys.stdout.flush()
 
         self.save_results(results)
 
