d7ad86e72d9b4b261badc18d551bd7eda8ef6079
Abhishek Mall <mallabhi@ada01.bc.rzg.mpg.de>
2024-08-13 11:07:03 +0200

    Added config functions

--- utils.py
+++ utils.py
@@ -1,15 +1,36 @@
-import numpy as np
-import h5py
 import configparser
+import ast
 
-def load_config(config_file):
+
+def makedata_config(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
+
     N = config.getint('DATA_GENERATION', 'N')
+    NUM_SAMPLES = config.getint('DATA_GENERATION', 'NUM_SAMPLES')
     SCALE = config.getint('DATA_GENERATION', 'SCALE')
+    SEED = config.getint('DATA_GENERATION', 'SEED')
     DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
-    OUTPUT_FILE = config.get('OPTIMIZATION', 'OUTPUT_FILE', fallback=None)
-    return N, DATA_FILE, SCALE, OUTPUT_FILE
+    SHAPE = config.get('DATA_GENERATION', 'SHAPE')
+    SHIFTS = ast.literal_eval(config['DATA_GENERATION']['SHIFTS'])
+    FLUENCE = ast.literal_eval(config['DATA_GENERATION']['FLUENCE'])
+
+    return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, FLUENCE, SEED, DATA_FILE
+
+def optim_config(config_file):
+    config = configparser.ConfigParser()
+    config.read(config_file)
+
+    N = config.getint('DATA_GENERATION', 'N')
+    NUM_SAMPLES = config.getint('DATA_GENERATION', 'NUM_SAMPLES')
+    SCALE = config.getint('DATA_GENERATION', 'SCALE')
+    SEED = config.getint('DATA_GENERATION', 'SEED')
+    DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
+
+    INIT_FTOBJ = config.get('OPTIMIZATION', 'INIT_FTOBJ')
+    OUTPUT_FILE = config.get('OPTIMIZATION', 'OUTPUT_FILE')
+
+    return N, NUM_SAMPLES, SCALE, SEED, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE
 
 def get_vals(array, cen, h, k):
     qh = h + cen
