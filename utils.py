3f0022c652b4ba80d1a38456aefc874606cb913c
Abhishek Mall <mallabhi@ada01.bc.rzg.mpg.de>
2024-09-25 09:07:11 +0200

    added do_fft, do_ifft

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import configparser
 import ast
-
+import numpy as np
 
 def makedata_config(config_file):
     config = configparser.ConfigParser()
@@ -15,7 +15,11 @@ def makedata_config(config_file):
     SHIFTS = ast.literal_eval(config['DATA_GENERATION']['SHIFTS'])
     FLUENCE = ast.literal_eval(config['DATA_GENERATION']['FLUENCE'])
 
-    return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, FLUENCE, SEED, DATA_FILE
+    ADD_NOISE = config.getboolean('DATA_GENERATION', 'ADD_NOISE')
+    NOISE_MU = config.getfloat('DATA_GENERATION', 'NOISE_MU')
+    NOISE_K = config.getfloat('DATA_GENERATION', 'NOISE_K')
+
+    return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, FLUENCE, SEED, DATA_FILE, ADD_NOISE, NOISE_MU, NOISE_K
 
 def optim_config(config_file):
     config = configparser.ConfigParser()
@@ -30,10 +34,18 @@ def optim_config(config_file):
     INIT_FTOBJ = config.get('OPTIMIZATION', 'INIT_FTOBJ')
     OUTPUT_FILE = config.get('OPTIMIZATION', 'OUTPUT_FILE')
     PIXELS = config.getint('OPTIMIZATION', 'PIXELS')
+
     return N, NUM_SAMPLES, SCALE, SEED, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE, PIXELS
 
 def get_vals(array, cen, h, k):
     qh = h + cen
     qk = k + cen
     return array[qh, qk]
+
+
+def do_fft(obj):
+    return np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(obj)))
+
+def do_ifft(ftobj):
+    return np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(ftobj)))
 
