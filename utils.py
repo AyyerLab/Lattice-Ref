56c8dedfe10ea0f046d10a70ea90a510bd4069bc
Abhishek Mall <mallabhi@ada01.bc.rzg.mpg.de>
2024-10-15 12:35:54 +0200

    added init_obj

--- utils.py
+++ utils.py
@@ -2,6 +2,26 @@ import configparser
 import ast
 import numpy as np
 
+def get_vals(array, cen, h, k):
+    qh = h + cen
+    qk = k + cen
+    return array[qh, qk]
+
+def do_fft(obj):
+    return np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(obj)))
+
+def do_ifft(ftobj):
+    return np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(ftobj)))
+
+def init_tobj(N, pixels):
+    size = (N, N)
+    obj = np.zeros(size)
+    cen = (N // 2, N // 2)
+    y, x = np.ogrid[:N, :N]
+    dcen = np.sqrt((x - cen[1])**2 + (y - cen[0])**2)
+    obj.ravel()[np.argsort(dcen.ravel())[:pixels]] = 1
+    return obj
+
 def makedata_config(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
@@ -21,31 +41,19 @@ def makedata_config(config_file):
 
     return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, FLUENCE, SEED, DATA_FILE, ADD_NOISE, NOISE_MU, NOISE_K
 
-def optim_config(config_file):
+def run_config(config_file):
     config = configparser.ConfigParser()
     config.read(config_file)
 
     N = config.getint('DATA_GENERATION', 'N')
-    NUM_SAMPLES = config.getint('DATA_GENERATION', 'NUM_SAMPLES')
-    SCALE = config.getint('DATA_GENERATION', 'SCALE')
-    SEED = config.getint('DATA_GENERATION', 'SEED')
-    DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
-
+    NUM_ITER = config.getint('OPTIMIZATION', 'NUM_ITERATION')
     INIT_FTOBJ = config.get('OPTIMIZATION', 'INIT_FTOBJ')
+
+    DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
     OUTPUT_FILE = config.get('OPTIMIZATION', 'OUTPUT_FILE')
     PIXELS = config.getint('OPTIMIZATION', 'PIXELS')
+    USE_SHRINKWRAP = config.getboolean('OPTIMIZATION', 'USE_SHRINKWRAP')
+    SHRINKWRAP_RUNS = ast.literal_eval(config['OPTIMIZATION']['SHRINKWRAP_RUNS'])
 
-    return N, NUM_SAMPLES, SCALE, SEED, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE, PIXELS
-
-def get_vals(array, cen, h, k):
-    qh = h + cen
-    qk = k + cen
-    return array[qh, qk]
-
-
-def do_fft(obj):
-    return np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(obj)))
-
-def do_ifft(ftobj):
-    return np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(ftobj)))
+    return N, NUM_ITER, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE, PIXELS, USE_SHRINKWRAP, SHRINKWRAP_RUNS
 
