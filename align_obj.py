1d1cb6abd556a0c083785bbc85a7787df181708d
Abhishek Mall <mallabhi@mpsd-hpc-login1.desy.de>
2024-11-14 12:13:17 +0100

    refactored code

--- align_obj.py
+++ align_obj.py
@@ -2,15 +2,24 @@ import numpy as np
 import h5py
 from scipy import ndimage
 from scipy.optimize import curve_fit
+import configparser
 
 class Align:
-    def __init__(self, ftobj_path, fitted_ftobj_path):
-        self.N = 127
-        with h5py.File(ftobj_path, 'r') as f:
-            self.ftobj = f['ftobj'][:]
+    def __init__(self, fitted_ftobj_path, ftobj_path, config_path):
+        config = configparser.ConfigParser()
+        config.read(config_path)
+        
+        self.N = int(config['DATA_GENERATION']['N'])
+        self.PIXELS = int(config['OPTIMIZATION']['PIXELS'])
+        self.SIGMA = float(config['ALIGN']['SIGMA'])
+        self.REGION = tuple(map(int, config['ALIGN']['REGION'].split(',')))
+
+        # Load data
         with h5py.File(fitted_ftobj_path, 'r') as f:
             self.fitted_ftobj = f['fitted_ftobj'][:]
-    
+        with h5py.File(ftobj_path, 'r') as f:
+            self.ftobj = f['ftobj'][:]
+
     def do_fft(self, obj):
         return np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(obj)))
 
@@ -18,7 +27,6 @@ class Align:
         return np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(ftobj)))
 
     def _get_supp(self, ftobj, sig, pixels):
-        # Get estimate of support
         invsuppmask = np.ones((self.N,) * 2, dtype=np.bool_)
         amodel = np.real(self.do_ifft(ftobj.reshape((self.N,) * 2)))
         samodel = ndimage.gaussian_filter(amodel, sig)
@@ -27,7 +35,6 @@ class Align:
         return invsuppmask
 
     def center_obj(self, obj, clean_obj):
-        # Center the object based on the centroid of clean_obj
         x, y = np.indices(clean_obj.shape)
         cx = (np.abs(clean_obj) * x).sum() / np.abs(clean_obj).sum()
         cy = (np.abs(clean_obj) * y).sum() / np.abs(clean_obj).sum()
@@ -35,28 +42,21 @@ class Align:
         return obj_cen
 
     def fit_pramp(self, pramp):
-        # Fit lunear function to phase ramp
         def linear_fun(coords, A, B, C):
             x, y = coords
             return A * x + B * y + C
+
         h, w = pramp.shape
         x = np.arange(w) - w // 2
         y = np.arange(h) - h // 2
         X, Y = np.meshgrid(x, y)
-        x_flat = X.flatten()
-        y_flat = Y.flatten()
+        coords = np.vstack((X.flatten(), Y.flatten()))
         z_flat = pramp.flatten()
-        coords = np.vstack((x_flat, y_flat))
         params, _ = curve_fit(linear_fun, coords, z_flat)
         return params
 
     def fix_phase(self, fobj, pramp, region=None):
-        # Use a specific region for fitting if provided
-        if region is not None:
-            y1, y2, x1, x2 = region
-            pramp_region = pramp[y1:y2, x1:x2]
-        else:
-            pramp_region = pramp
+        pramp_region = pramp if region is None else pramp[region[0]:region[1], region[2]:region[3]]
         params = self.fit_pramp(pramp_region)
         A, B, C = params
         h, w = pramp.shape
@@ -64,33 +64,29 @@ class Align:
         y = np.arange(h) - h // 2
         X, Y = np.meshgrid(x, y)
         fit_pramp_vals = (A * X + B * Y + C)
-        # Apply phase ramp
         fobj_align = fobj * np.exp(1j * fit_pramp_vals)
         return fobj_align
 
-    def align(self, sig=1.25, pixels=830, region=(40, 85, 45, 80)):
+    def align(self):
         tobj = np.abs(self.do_ifft(self.ftobj))
         fitted_tobj = np.abs(self.do_ifft(self.fitted_ftobj))
 
-        # Compute support mask
         fitted_tobj_copy = fitted_tobj.copy()
-        support = self._get_supp(self.fitted_ftobj, sig, pixels)
+        support = self._get_supp(self.fitted_ftobj, self.SIGMA, self.PIXELS)
         fitted_tobj_copy[support] = 0
-        # Center the objects
+
         fitted_tobj_cen = self.center_obj(fitted_tobj, fitted_tobj_copy)
         tobj_cen = self.center_obj(tobj, tobj)
-        # Compute pramp
+
         pramp = np.angle(self.do_fft(tobj_cen) / self.do_fft(fitted_tobj_cen))
-        # Fix the phase
-        fitted_ftobj_align = self.fix_phase(self.do_fft(fitted_tobj_cen), pramp, region)
+        fitted_ftobj_align = self.fix_phase(self.do_fft(fitted_tobj_cen), pramp, self.REGION)
         return fitted_ftobj_align, self.do_fft(tobj_cen)
 
-####  Execution
 
-#ftobj_path = '/scratch/mallabhi/lattice_ref/data/K/dataset_K10.h5'
-#fitted_ftobj_path = '/scratch/mallabhi/lattice_ref/output/output_K10_NS_150.h5'
-#aligner = ao.Align(ftobj_path, fitted_ftobj_path)
+
+# IPYTHON EXECUTION
+#ftobj_path = '/path/to/dataset.h5'
+#fitted_ftobj_path = '/path/to/fitted_dataset.h5'
+#aligner = Align(ftobj_path, fitted_ftobj_path)
 #aligned_fitted_ftobj, aligned_ftobj = aligner.align()
-
-
 
--- calc_frc.py
+++ calc_frc.py
@@ -1,97 +1,34 @@
-import h5py
 import numpy as np
+import h5py
+
 from scipy import ndimage
 from scipy.optimize import curve_fit
+import configparser
+
+from align_obj import Align
 
 class FRC:
-    def __init__(self, fitted_ftobj_path, ftobj_path):
-        self.N = 127
+    def __init__(self, fitted_ftobj_path, ftobj_path, config_path):
 
-        with h5py.File(fitted_ftobj_path, 'r') as f:
-            self.fitted_ftobj = f['fitted_ftobj'][:]
-        with h5py.File(ftobj_path, 'r') as f:
-            self.ftobj = f['ftobj'][:]
-    
-    def do_fft(self, obj):
-        return np.fft.fftshift(np.fft.fftn(np.fft.ifftshift(obj)))
+        self.ALIGN_OBJ = Align(fitted_ftobj_path, 
+                               ftobj_path, 
+                               config_path)
 
-    def do_ifft(self, ftobj):
-        return np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(ftobj)))
+    def calculate_frc(self, obj1, obj2, binsize=1.0, do_abs=False):
+        fobj1 = self.ALIGN_OBJ.do_fft(obj1)
+        fobj2 = self.ALIGN_OBJ.do_fft(obj2)
 
-    def _get_supp(self, ftobj, sig, pixels):
-        invsuppmask = np.ones((self.N,) * 2, dtype=np.bool_)
-        amodel = np.real(self.do_ifft(ftobj.reshape((self.N,) * 2)))
-        samodel = ndimage.gaussian_filter(amodel, sig)
-        thresh = np.quantile(samodel, (samodel.size - pixels) / samodel.size)
-        invsuppmask = samodel < thresh
-        return invsuppmask
+        x, y = np.meshgrid(np.arange(self.ALIGN_OBJ.N) - self.ALIGN_OBJ.N // 2,
+                           np.arange(self.ALIGN_OBJ.N) - self.ALIGN_OBJ.N // 2, 
+                           indexing='ij')
 
-    def center_obj(self, obj, clean_obj):
-        # Center the object based on the centroid of clean_obj
-        x, y = np.indices(clean_obj.shape)
-        cx = (np.abs(clean_obj) * x).sum() / np.abs(clean_obj).sum()
-        cy = (np.abs(clean_obj) * y).sum() / np.abs(clean_obj).sum()
-        obj_cen = np.roll(np.abs(obj), (self.N//2 - int(cx), self.N//2 - int(cy)), axis=(0, 1))
-        return obj_cen
-
-    def fit_pramp(self, pramp):
-        def linear_fun(coords, A, B, C):
-            x, y = coords
-            return A * x + B * y + C
-        h, w = pramp.shape
-        x = np.arange(w) - w // 2
-        y = np.arange(h) - h // 2
-        X, Y = np.meshgrid(x, y)
-        x_flat = X.flatten()
-        y_flat = Y.flatten()
-        z_flat = pramp.flatten()
-        coords = np.vstack((x_flat, y_flat))
-        params, _ = curve_fit(linear_fun, coords, z_flat)
-        return params
-    
-    def fix_phase(self, fobj, pramp, region=None):
-        # Use a specific region for fitting if provided
-        if region is not None:
-            y1, y2, x1, x2 = region
-            pramp_region = pramp[y1:y2, x1:x2]
-        else:
-            pramp_region = pramp
-        params = self.fit_pramp(pramp_region)
-        A, B, C = params
-        h, w = pramp.shape
-        x = np.arange(w) - w // 2
-        y = np.arange(h) - h // 2
-        X, Y = np.meshgrid(x, y)
-        fit_pramp_vals = (A * X + B * Y + C)
-        fobj_align = fobj * np.exp(1j * fit_pramp_vals)
-        return fobj_align
-
-    def align(self, sig=1.25, pixels=830, region=(40, 85, 45, 80)):
-        tobj = np.abs(self.do_ifft(self.ftobj))
-        fitted_tobj = np.abs(self.do_ifft(self.fitted_ftobj))
-
-        # Compute support mask
-        fitted_tobj_copy = fitted_tobj.copy()
-        support = self._get_supp(self.fitted_ftobj, sig, pixels)
-        fitted_tobj_copy[support] = 0
-        # Center the objects
-        fitted_tobj_cen = self.center_obj(fitted_tobj, fitted_tobj_copy)
-        tobj_cen = self.center_obj(tobj, tobj)
-        # Compute pramp
-        pramp = np.angle(self.do_fft(tobj_cen) / self.do_fft(fitted_tobj_cen))
-        # Fix the phase
-        fitted_ftobj_align = self.fix_phase(self.do_fft(fitted_tobj_cen), pramp, region)
-        return fitted_ftobj_align, self.do_fft(tobj_cen)    
-
-    def calculate_frc(self, obj1, obj2, binsize=1., do_abs=False):
-        fobj1 = self.do_fft(obj1)
-        fobj2 = self.do_fft(obj2)
-        x, y = np.meshgrid(np.arange(self.N) - self.N // 2, np.arange(self.N) - self.N // 2, indexing='ij')
         binrad = (np.sqrt(x**2 + y**2) / binsize).astype(int)
         rsize = binrad.max() + 1
+
         numr = np.zeros(rsize, dtype=np.complex128)
         denr1 = np.zeros(rsize, dtype=np.float64)
         denr2 = np.zeros(rsize, dtype=np.float64)
+
         np.add.at(numr, binrad, fobj1 * np.conj(fobj2))
         np.add.at(denr1, binrad, np.abs(fobj1)**2)
         np.add.at(denr2, binrad, np.abs(fobj2)**2)
@@ -99,6 +36,7 @@ class FRC:
         denr = np.sqrt(denr1 * denr2)
         frc_vals = np.zeros(rsize, dtype=np.float64)
         valid = denr > 0
+
         if do_abs:
             frc_vals[valid] = np.abs(numr[valid]) / denr[valid]
         else:
@@ -107,13 +45,8 @@ class FRC:
         return rvals, frc_vals
 
     def process(self):
-        aligned_fitted_ftobj, aligned_ftobj = self.align()
-        rvals, frc_vals = self.calculate_frc(self.do_ifft(aligned_fitted_ftobj), self.do_ifft(aligned_ftobj))
+        aligned_fitted_ftobj, aligned_ftobj = self.ALIGN_OBJ.align()
+        rvals, frc_vals = self.calculate_frc(self.ALIGN_OBJ.do_ifft(aligned_fitted_ftobj),
+                                             self.ALIGN_OBJ.do_ifft(aligned_ftobj))
         return rvals, frc_vals
-
-
-#### Execution
-#frc = FRC('/u/mallabhi/lattice_ref/data/output/output150.h5',
-#                            '/u/mallabhi/lattice_ref/data/dataset.h5')
-#rvals, frc_vals = frc.process()
 
