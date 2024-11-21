65f1901d767030008e2e7b30ac1380145ee0422d
Abhishek Mall <mallabhi@mpsd-hpc-gpu-005.desy.de>
2024-11-21 13:46:53 +0100

    removed explicit fluence range

--- utils.py
+++ utils.py
@@ -28,18 +28,17 @@ def makedata_config(config_file):
 
     N = config.getint('DATA_GENERATION', 'N')
     NUM_SAMPLES = config.getint('DATA_GENERATION', 'NUM_SAMPLES')
-    SCALE = config.getint('DATA_GENERATION', 'SCALE')
+    SCALE = config.getfloat('DATA_GENERATION', 'SCALE')
     SEED = config.getint('DATA_GENERATION', 'SEED')
     DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
     SHAPE = config.get('DATA_GENERATION', 'SHAPE')
     SHIFTS = ast.literal_eval(config['DATA_GENERATION']['SHIFTS'])
-    FLUENCE = ast.literal_eval(config['DATA_GENERATION']['FLUENCE'])
 
     ADD_NOISE = config.getboolean('DATA_GENERATION', 'ADD_NOISE')
     NOISE_MU = config.getfloat('DATA_GENERATION', 'NOISE_MU')
     NOISE_K = config.getfloat('DATA_GENERATION', 'NOISE_K')
 
-    return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, FLUENCE, SEED, DATA_FILE, ADD_NOISE, NOISE_MU, NOISE_K
+    return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, SEED, DATA_FILE, ADD_NOISE, NOISE_MU, NOISE_K
 
 def run_config(config_file):
     config = configparser.ConfigParser()
@@ -53,7 +52,6 @@ def run_config(config_file):
     OUTPUT_FILE = config.get('OPTIMIZATION', 'OUTPUT_FILE')
     PIXELS = config.getint('OPTIMIZATION', 'PIXELS')
     USE_SHRINKWRAP = config.getboolean('OPTIMIZATION', 'USE_SHRINKWRAP')
-    SHRINKWRAP_RUNS = ast.literal_eval(config['OPTIMIZATION']['SHRINKWRAP_RUNS'])
 
-    return N, NUM_ITER, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE, PIXELS, USE_SHRINKWRAP, SHRINKWRAP_RUNS
+    return N, NUM_ITER, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE, PIXELS, USE_SHRINKWRAP
 
