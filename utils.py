cd02d94e0d9f679512e1466907ce8ca5eef7a486
Abhishek Mall <mallabhi@mpsd-hpc-gpu-005.desy.de>
2025-02-03 10:32:18 +0100

    added func.

--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
-import configparser
-import ast
 import numpy as np
+from scipy.ndimage import map_coordinates
 
 def get_vals(array, cen, h, k):
     qh = h + cen
@@ -13,52 +12,22 @@ def do_fft(obj):
 def do_ifft(ftobj):
     return np.fft.fftshift(np.fft.ifftn(np.fft.ifftshift(ftobj)))
 
+def rotate_arr(N, arr, angle):
+    cen = N // 2
+    qh, qk = np.indices((N, N))
+    qh -= cen
+    qk -= cen
+    qh_rot = np.cos(angle) * qh - np.sin(angle) * qk
+    qk_rot = np.sin(angle) * qh + np.cos(angle) * qk
+    coords = np.array([qh_rot + cen, qk_rot + cen])
+    rotated_arr = map_coordinates(arr, coords, order=1, mode='nearest')
+    return rotated_arr
+
+
 def init_tobj(N, pixels):
-    size = (N, N)
-    obj = np.zeros(size)
+    obj = np.zeros((N,N))
     cen = (N // 2, N // 2)
     y, x = np.ogrid[:N, :N]
     dcen = np.sqrt((x - cen[1])**2 + (y - cen[0])**2)
     obj.ravel()[np.argsort(dcen.ravel())[:pixels]] = 1
     return obj
-
-def makedata_config(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-
-    N = config.getint('DATA_GENERATION', 'N')
-    NUM_SAMPLES = config.getint('DATA_GENERATION', 'NUM_SAMPLES')
-    SCALE = config.getfloat('DATA_GENERATION', 'SCALE')
-    SEED = config.getint('DATA_GENERATION', 'SEED')
-    DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
-    SHAPE = config.get('DATA_GENERATION', 'SHAPE')
-    SHIFTS = ast.literal_eval(config['DATA_GENERATION']['SHIFTS'])
-    ANGLES = ast.literal_eval(config['DATA_GENERATION']['ANGLES'])
-
-    APPLY_ORIENTATION = config.getboolean('DATA_GENERATION', 'APPLY_ORIENTATION')
-
-    ADD_NOISE = config.getboolean('DATA_GENERATION', 'ADD_NOISE')
-    NOISE_MU = config.getfloat('DATA_GENERATION', 'NOISE_MU')
-    NOISE_K = config.getfloat('DATA_GENERATION', 'NOISE_K')
-
-    return N, NUM_SAMPLES, SHAPE, SCALE, SHIFTS, SEED, DATA_FILE, ADD_NOISE, NOISE_MU, NOISE_K, APPLY_ORIENTATION, ANGLES
-
-def run_config(config_file):
-    config = configparser.ConfigParser()
-    config.read(config_file)
-
-    N = config.getint('DATA_GENERATION', 'N')
-    NUM_ITER = config.getint('OPTIMIZATION', 'NUM_ITERATION')
-    NUM_SAMPLES = config.getint('DATA_GENERATION', 'NUM_SAMPLES')
-    SEED = config.getint('DATA_GENERATION', 'SEED')
-    INIT_FTOBJ = config.get('OPTIMIZATION', 'INIT_FTOBJ')
-
-    DATA_FILE = config.get('DATA_GENERATION', 'DATA_FILE')
-    OUTPUT_FILE = config.get('OPTIMIZATION', 'OUTPUT_FILE')
-    PIXELS = config.getint('OPTIMIZATION', 'PIXELS')
-    USE_SHRINKWRAP = config.getboolean('OPTIMIZATION', 'USE_SHRINKWRAP')
-    APPLY_ORIENTATION = config.getboolean('DATA_GENERATION', 'APPLY_ORIENTATION')
-    ANGLES = ast.literal_eval(config['DATA_GENERATION']['ANGLES'])
-
-    return N, NUM_SAMPLES, NUM_ITER, SEED, INIT_FTOBJ, DATA_FILE, OUTPUT_FILE, PIXELS, USE_SHRINKWRAP, APPLY_ORIENTATION, ANGLES
-
