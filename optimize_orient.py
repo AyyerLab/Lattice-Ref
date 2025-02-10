122b60f11a600f57173fd2ab76bd5e5157558f48
Abhishek Mall <mallabhi@mpsd-hpc-gpu-005.desy.de>
2025-02-10 09:10:47 +0100

    upped/lower bound decreases by factor 2 every step

--- optimize_orient.py
+++ optimize_orient.py
@@ -1,8 +1,8 @@
-import cupy as cp
 import h5py
 import argparse
-from configparser import ConfigParser
+import cupy as cp
 from utils_cupy import rotate_arr
+from configparser import ConfigParser
 
 
 class OrientOptimizer:
@@ -18,7 +18,7 @@ class OrientOptimizer:
         self.data_file = data_file
 
         self.fluence = cp.asarray(fluence)
-        self.NUM_SAMPLES = len(self.fluence)
+        self.NUM_FRAMES = len(self.fluence)
         self.shifts = cp.asarray(shifts)
         self.ftobj = cp.asarray(ftobj)
 
@@ -31,14 +31,14 @@ class OrientOptimizer:
             self.intens_vals = cp.asarray(f['intens'][:])
 
     def optimize_orientation(self):
-        optimal_angs = cp.zeros(self.NUM_SAMPLES)
-        steps = cp.zeros(self.NUM_SAMPLES)
+        optimal_angs = cp.zeros(self.NUM_FRAMES)
+        steps = cp.zeros(self.NUM_FRAMES)
 
         hk_indices = (self.hk[:, 0] + self.cen, self.hk[:, 1] + self.cen)
 
         angle_threshold = 1e-4
 
-        for i in range(self.NUM_SAMPLES):
+        for i in range(self.NUM_FRAMES):
             dx = self.shifts[:, 0][i]
             dy = self.shifts[:, 1][i]
             fluence = self.fluence[i]
@@ -69,7 +69,7 @@ class OrientOptimizer:
             optimal_ang_coarse = ang_coarse_range[cp.argmin(objective_values_coarse)]
 
             coarse_steps = len(ang_coarse_range)
-            total_steps = coarse_steps
+            total_steps = 1
 
             # Refinement
             current_ang = optimal_ang_coarse
@@ -77,11 +77,12 @@ class OrientOptimizer:
 
             fine_step_size = self.fine_step_size
             previous_ang = float('inf')
+            w = 0.02
 
             while cp.abs(current_ang - previous_ang) > angle_threshold:
-
-                lower_bound = cp.maximum(current_ang - 0.02, 0)
-                upper_bound = cp.minimum(current_ang + 0.02, 2 * cp.pi)
+                total_steps += 1
+                lower_bound = cp.maximum(current_ang - w, 0)
+                upper_bound = cp.minimum(current_ang + w, 2 * cp.pi)
                 ang_fine_range = cp.arange(lower_bound, upper_bound, fine_step_size)
 
                 fine_objective_values = cp.array([objective_ang(ang) for ang in ang_fine_range])
@@ -92,14 +93,12 @@ class OrientOptimizer:
                 current_ang = optimal_ang_fine
                 min_error_current = min_error_fine
 
-                total_steps += len(ang_fine_range)
                 fine_step_size /= 2.0
+                w /= 2.0
 
             optimal_angs[i] = current_ang
             steps[i] = total_steps
-
-            print(f"Frame {i}:  Best Angle: {current_ang}, Steps: {total_steps}, Error: {min_error_current}", flush=True)
-
+            print(f"Frame {i+1}:  Best Angle: {current_ang}, Steps: {total_steps}", flush=True)
         return optimal_angs, steps
 
 
@@ -124,7 +123,7 @@ if __name__ == "__main__":
 
     optimizer = OrientOptimizer(N, fluence, shifts, ftobj, data_file, fine_step_size)
     optimal_angs, steps = optimizer.optimize_orientation()
-    with h5py.File('/scratch/mallabhi/lattice_ref/output/optimize_orient_RIB_10Angs.h5', "w") as f:
+    with h5py.File('/scratch/mallabhi/lattice_ref/output/optimize_orient_PS1_10Angs.h5', "w") as f:
         f['fitted_angles'] = optimal_angs.get()
     print("Orientation Optimization Complete.")
 
