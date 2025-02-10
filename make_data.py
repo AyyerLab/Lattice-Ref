36ae77980ab43cb4cab075043855cc21b7077f12
Abhishek Mall <mallabhi@mpsd-hpc-gpu-005.desy.de>
2025-02-10 09:08:14 +0100

    apply_orientation flag  option

--- make_data.py
+++ make_data.py
@@ -1,31 +1,23 @@
-import numpy as np
-from scipy import ndimage
-import h5py
-
-import mrcfile
-import configparser
+import os
 import ast
-
+import h5py
+import mrcfile
+import numpy as np
+import configparser
+from scipy import ndimage
+from utils import rotate_arr, do_fft, do_ifft
 from config_functions import makedata_config
-from utils import do_fft, do_ifft
-from utils import rotate_arr
 
 class DataGenerator:
-    def __init__(self, n, scale, num_frames, seed, shifts, sample, fluence_mode, apply_orientation, add_noise, noise_mu, noise_k, data_file):
-        self.N = N
-        self.NUM_FRAMES = num_frames
-        self.SAMPLE = sample
+    def __init__(self, n, scale, num_frames, seed, sample, add_noise, noise_k, data_file):
+        self.N = n
         self.SCALE = scale
+        self.NUM_FRAMES = num_frames
         self.SEED = seed
-        self.SHIFTS = shifts
-        self.FLUENCE_MODE = fluence_mode
+        self.SAMPLE = sample
 
         self.ADD_NOISE = add_noise
-        self.NOISE_MU = noise_mu
         self.NOISE_K = noise_k
-
-        self.APPLY_ORIENTATION = apply_orientation
-
         self.DATA_FILE = data_file
 
         self.cen = self.N // 2
@@ -33,22 +25,16 @@ class DataGenerator:
         self.qh -= self.cen
         self.qk -= self.cen
 
-        if self.FLUENCE_MODE == "distribution":
-            xvals = np.linspace(0, 1, 500)
-            self.max_fl = self.pfluence(xvals[1:]).max()
-            np.random.seed(self.SEED)
-            self.fluence_vals = self.randfluence(self.NUM_FRAMES)
-        else:
-            raise ValueError(f"Invalid FLUENCE mode: {FLUENCE_MODE}")
+        xvals = np.linspace(0, 1, 500)
+        self.max_fl = self.pfluence(xvals[1:]).max()
+        np.random.seed(self.SEED)
+        self.fluence_vals = self.randfluence(self.NUM_FRAMES)
 
         np.random.seed(self.SEED)
-        self.dx_vals = np.random.uniform(self.SHIFTS[0], self.SHIFTS[1], size=self.NUM_FRAMES)
-        self.dy_vals = np.random.uniform(self.SHIFTS[0], self.SHIFTS[1], size=self.NUM_FRAMES)
-
-        if self.APPLY_ORIENTATION:
-            np.random.seed(self.SEED)
-            self.angles = np.random.uniform(0, 1, size=self.NUM_FRAMES) * 2. * np.pi
-            #self.angles = np.random.uniform(0, 1, size=self.NUM_FRAMES) * (2. * np.pi / 3)
+        self.dx_vals = np.random.uniform(0.01, 1, size=self.NUM_FRAMES)
+        self.dy_vals = np.random.uniform(0.01, 1, size=self.NUM_FRAMES)
+        #self.angles = np.random.uniform(0, 1, size=self.NUM_FRAMES) * (2. * np.pi / 3.)
+        self.angles = np.random.uniform(0, 1, size=self.NUM_FRAMES) * 2. * np.pi 
 
     def pfluence(self, xvals):
         return np.sqrt(-np.log(xvals)) * (1 - np.exp(-xvals * 200))
@@ -72,13 +58,16 @@ class DataGenerator:
         x, y = np.indices((size, size), dtype='f8')
         mask = np.zeros((size, size), dtype='f8')
 
-        if self.SAMPLE in ['PS1_5Angs','PS1_10Angs','RIB_5Angs','RIB_10Angs']:
-            file_paths = {'PS1_5Angs': '/scratch/mallabhi/lattice_ref/data/PS1_map.mrc',
-                          'PS1_10Angs': '/scratch/mallabhi/lattice_ref/data/PS1_10angs.mrc',
-                          'RIB_5Angs': '/scratch/mallabhi/lattice_ref/data/rib.mrc',
-                          'RIB_10Angs': '/scratch/mallabhi/lattice_ref/data/Rib_10angs.mrc'}
-            target_file = file_paths[self.SAMPLE]
-            with mrcfile.open(target_file, permissive=True) as mrc:
+        BASE_DATA_PATH = '/scratch/mallabhi/lattice_ref/data'
+        file_names = {
+            'PS1_5A': 'PS1_map.mrc',
+            'PS1_10A': 'PS1_10angs.mrc',
+            'RIB_5A': 'rib.mrc',
+            'RIB_10A': 'Rib_10angs.mrc'}
+
+        if self.SAMPLE in file_names:
+            file_path = os.path.join(BASE_DATA_PATH, file_names[self.SAMPLE])
+            with mrcfile.open(file_path, permissive=True) as mrc:
                 data = mrc.data
 
             proj = data.sum(0)
@@ -105,16 +94,12 @@ class DataGenerator:
         np.random.seed(self.SEED)
         random_ = np.random.rand(self.N, self.N)
         random_ = random_ > 0.7
-        #unitc = ndimage.gaussian_filter(random_.astype(float), sigma=(0.7, 0.7), mode='wrap') * (rad<30)
         unitc = ndimage.gaussian_filter(random_.astype(float), sigma=(0.7, 0.7), mode='wrap')
         return unitc, self.SCALE * do_fft(unitc)
 
     def generate_dataset(self):
-        # Target Object
         tobj, ftobj = self.target_obj()
-        # Unit Cell
         unitc, funitc = self.unit_cell()
-        # Generate Data
         intens_vals = np.zeros((self.NUM_FRAMES, self.N, self.N))
         noise_vals = None
         rotated_ftobjs = np.zeros((self.NUM_FRAMES, self.N, self.N), dtype=np.complex128)
@@ -128,19 +113,15 @@ class DataGenerator:
             fluence = self.fluence_vals[i]
             phase = self.phase_ramp(shiftx, shifty)
 
-            if self.APPLY_ORIENTATION:
-                ftobj_rot = rotate_arr(self.N, ftobj, self.angles[i])
-                ft = fluence * ftobj_rot * phase
-                rotated_ftobjs[i] = ftobj_rot
-            else:
-                ft = fluence * ftobj * phase
-
+            ftobj_rot = rotate_arr(self.N, ftobj, self.angles[i])
+            ft = fluence * ftobj_rot * phase
+            rotated_ftobjs[i] = ftobj_rot
             intens = np.abs(funitc + ft)**2
 
             if self.ADD_NOISE:
                 noise_sigma = self.NOISE_K * np.sqrt(intens)
                 np.random.seed(self.SEED)
-                noise = np.random.normal(loc=self.NOISE_MU, scale=noise_sigma, size=intens.SAMPLE)
+                noise = np.random.normal(loc=0, scale=noise_sigma, size=intens.SAMPLE)
                 noise_vals[i] = noise
                 intens_vals[i] = intens + noise
             else:
@@ -154,19 +135,17 @@ class DataGenerator:
         print(f"Saving dataset to {filename}")
         try:
             with h5py.File(filename, 'w') as f:
+                f.create_dataset('NOISE_K', data=self.NOISE_K)
+                f.create_dataset('SCALE', data=self.SCALE)
                 f.create_dataset('intens', data=intens_vals)
-                f.create_dataset('ftobj', data=ftobj)
                 f.create_dataset('rotated_ftobjs', data=rotated_ftobjs)
+                f.create_dataset('ftobj', data=ftobj)
                 f.create_dataset('tobj', data=tobj)
                 f.create_dataset('funitc', data=funitc)
                 f.create_dataset('unitc', data=unitc)
                 f.create_dataset('shifts', data=np.vstack((self.dx_vals, self.dy_vals)).T)
                 f.create_dataset('fluence', data=self.fluence_vals)
-                f.create_dataset('NOISE_K', data=self.NOISE_K)
-                f.create_dataset('SCALE', data=self.SCALE)
-
-                if self.APPLY_ORIENTATION:
-                    f.create_dataset('angles', data=self.angles)
+                f.create_dataset('angles', data=self.angles)
 
                 if self.ADD_NOISE:
                     f.create_dataset('noise', data=noise)
@@ -178,9 +157,7 @@ class DataGenerator:
 
 if __name__ == "__main__":
     config_file = 'config.ini'
-    ( N, SCALE, NUM_FRAMES, SEED, SHIFTS, SAMPLE, FLUENCE_MODE, APPLY_ORIENTATION, ADD_NOISE, NOISE_MU, NOISE_K, DATA_FILE) = makedata_config(config_file)
-
-    generator = DataGenerator( N, SCALE, NUM_FRAMES, SEED, SHIFTS, SAMPLE, FLUENCE_MODE, APPLY_ORIENTATION, ADD_NOISE, NOISE_MU, NOISE_K, DATA_FILE)
-
+    ( N, SCALE, NUM_FRAMES, SEED, SAMPLE, ADD_NOISE, NOISE_K, DATA_FILE) = makedata_config(config_file)
+    generator = DataGenerator( N, SCALE, NUM_FRAMES, SEED, SAMPLE, ADD_NOISE, NOISE_K, DATA_FILE)
     generator.save_data(DATA_FILE)
 
